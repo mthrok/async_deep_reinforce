8de434bf26a1fed51210d5538425211d43f79c8b
moto <mthrok@gmail.com>
2018-02-25 11:31:26 -0800

    Fetch action size automatically

--- constants.py
+++ constants.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from ale_python_interface import ALEInterface
+
+def _get_action_size(rom):
+    print(rom)
+    ale = ALEInterface()
+    ale.loadROM(rom)
+    n_actions = len(ale.getMinimalActionSet())
+    del ale
+    return n_actions
+
 LOCAL_T_MAX = 20 # repeat step size
 RMSP_ALPHA = 0.99 # decay parameter for RMSProp
 RMSP_EPSILON = 0.1 # epsilon parameter for RMSProp
 CHECKPOINT_DIR = 'checkpoints'
-LOG_FILE = 'tmp/a3c_log'
 INITIAL_ALPHA_LOW = 1e-4    # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 1e-2   # log_uniform high limit for learning rate
 
 PARALLEL_SIZE = 8 # parallel thread size
-ROM = "pong.bin"     # action size = 3
-ACTION_SIZE = 3 # action size
+ROM = "roms/breakout.bin"
+ACTION_SIZE = _get_action_size(ROM.encode('ascii'))
+LOG_FILE = 'a3c/%s' % ROM.split('.')[0].split('/')[1]
 
 INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 GAMMA = 0.99 # discount factor for rewards
