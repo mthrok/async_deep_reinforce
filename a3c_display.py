395246894028d954a73b24eb18dfd80b980c94d7
moto <mthrok@gmail.com>
2018-02-28 11:27:59 -0800

    Clean up display and vidualize scripts

--- a3c_display.py
+++ a3c_display.py
@@ -1,63 +1,45 @@
-# -*- coding: utf-8 -*-
-import tensorflow as tf
 import numpy as np
-import random
+import tensorflow as tf
 
+import constants
 from game_state import GameState
 from game_ac_network import GameACFFNetwork, GameACLSTMNetwork
-from a3c_training_thread import A3CTrainingThread
-from rmsprop_applier import RMSPropApplier
 
-from constants import ACTION_SIZE
-from constants import PARALLEL_SIZE
-from constants import CHECKPOINT_DIR
-from constants import RMSP_EPSILON
-from constants import RMSP_ALPHA
-from constants import GRAD_NORM_CLIP
-from constants import USE_GPU
-from constants import USE_LSTM
 
 def choose_action(pi_values):
-  return np.random.choice(range(len(pi_values)), p=pi_values)  
+    return np.random.choice(range(len(pi_values)), p=pi_values)
 
-# use CPU for display tool
-device = "/cpu:0"
 
-if USE_LSTM:
-  global_network = GameACLSTMNetwork(ACTION_SIZE, -1, device)
-else:
-  global_network = GameACFFNetwork(ACTION_SIZE, -1, device)
+def _main():
+    # use CPU for display tool
+    device = "/cpu:0"
 
-learning_rate_input = tf.placeholder("float")
+    Network = GameACLSTMNetwork if constants.USE_LSTM else GameACFFNetwork
+    global_network = Network(constants.ACTION_SIZE, -1, device)
 
-grad_applier = RMSPropApplier(learning_rate = learning_rate_input,
-                              decay = RMSP_ALPHA,
-                              momentum = 0.0,
-                              epsilon = RMSP_EPSILON,
-                              clip_norm = GRAD_NORM_CLIP,
-                              device = device)
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
 
-sess = tf.Session()
-init = tf.global_variables_initializer()
-sess.run(init)
+    saver = tf.train.Saver()
+    checkpoint = tf.train.get_checkpoint_state(constants.CHECKPOINT_DIR)
+    if checkpoint and checkpoint.model_checkpoint_path:
+        saver.restore(sess, checkpoint.model_checkpoint_path)
+        print("checkpoint loaded:", checkpoint.model_checkpoint_path)
+    else:
+        print("Could not find old checkpoint")
 
-saver = tf.train.Saver()
-checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
-if checkpoint and checkpoint.model_checkpoint_path:
-  saver.restore(sess, checkpoint.model_checkpoint_path)
-  print("checkpoint loaded:", checkpoint.model_checkpoint_path)
-else:
-  print("Could not find old checkpoint")
+    game_state = GameState(0, display=True, no_op_max=0)
 
-game_state = GameState(0, display=True, no_op_max=0)
+    while True:
+        pi_values = global_network.run_policy(sess, game_state.s_t)
 
-while True:
-  pi_values = global_network.run_policy(sess, game_state.s_t)
+        game_state.process(choose_action(pi_values))
+        if game_state.terminal:
+            game_state.reset()
+        else:
+            game_state.update()
 
-  action = choose_action(pi_values)
-  game_state.process(action)
 
-  if game_state.terminal:
-    game_state.reset()
-  else:
-    game_state.update()
+if __name__ == '__main__':
+    _main()
